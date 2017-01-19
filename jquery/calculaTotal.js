ce77e025d0440f7ae46039a01349146b78085913
LuizdosReis <luizdosreis@gmail.com>
2017-01-19 19:29:44 -0200

    mais seletores

--- jquery/calculaTotal.js
+++ jquery/calculaTotal.js
@@ -1,6 +1,25 @@
+var umaPropaganda = function(){
+	var propagandas = ["O que acha de comprar uma motocicleta?",
+               "O que acha de comprar uma lancha?",
+               "O que acha de comprar uma bicicleta?",
+               "O que acha de comprar uma carro?"
+               ];
+    var texto = propagandas[Math.floor(propagandas.length*Math.random())];
+    var tr = $("<tr>").addClass("propaganda").append($("<td>"));
+    tr.find("td").attr("colspan",6).text(texto);
+    return tr;	
+}
+
 $(function(){
 	calculaTotal();
+	$(".carrinho").each(function(){
+		$(this).find("tr:nth-child(3n)").each(function(){
+			umaPropaganda().insertAfter($(this));
+		});
+	});
 });
+
+
 
 var calculaTotal = function(){
 	var carrinhos = $(".carrinho");
